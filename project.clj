07fbdd9fef6f4fc598b4ebf4c80e510db85c4b1f
Nola Stowe <nola.stowe@ge.com>
2015-10-24 20:59:39 -0500

    updated project to have info for github

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.3.1"]                 
                  [clj-yaml "0.4.0"]
-                 [clj-tosca "0.1.0-SNAPSHOT"]
+                 [electric-it/clj-tosca "1.0"]
                  [com.amazonaws/aws-lambda-java-core "1.0.0"]
                  [amazonica "0.3.34"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
