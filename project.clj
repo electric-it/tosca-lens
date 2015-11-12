0c7050dfc950d1b2c2abe666bdcca530cfd1b629
Nola Stowe <nola.stowe@ge.com>
2015-11-12 14:19:20 -0600

    added license and info to project

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject tosca-lens "0.1.0-SNAPSHOT"
   :description "Given a machine id and attribute examine the state."
+  :license "Apache Version 2.0"
+  :url "https://github.com/electric-it/tosca-lens"
+  :scm {:name "git"
+        :url "https://github.com/electric-it/tosca-lens"}  
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.amazonaws/aws-lambda-java-core "1.0.0"]
                  [clj-yaml "0.4.0"]
