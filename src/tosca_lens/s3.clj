8857b5458617fd6072d94e295355585dfb3dc4cd
Nola Stowe <nola.stowe@ge.com>
2016-01-13 16:25:43 -0600

    work on adding tags for s3

--- src/tosca_lens/s3.clj
+++ src/tosca_lens/s3.clj
@@ -18,12 +18,12 @@
   "Build a tosca document to describe an s3 resource"
   [audit-params]
   (let [tags (get-data (:bucket-name audit-params))
-        meta (get-metadata (:bucket-name audit-params))
+        
         format (get-in audit-params [:format] "json")
         node (node/build "tosca.nodes.S3Storage")
         nodei (-> (nodei/build)
                   (nodei/add-property "tags" tags)
-                  (nodei/add-property "ownerName" (:display-name (:owner meta)))
-                  (nodei/add-property "ownerID" (:id (:owner meta))))]
+                  
+                  )]
     (-> (template/build nodei node)
         (template/publish format))))
