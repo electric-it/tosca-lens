9efa1309da66bd3ff7832829663d5c3336ba1d9d
Nola Stowe <nola.stowe@ge.com>
2016-01-29 12:57:57 -0600

    removed lambda to make this standalone library

--- project.clj
+++ project.clj
@@ -1,16 +1,13 @@
 (defproject electric-it/tosca-lens "1.0.0"
-  :description "Given a machine id and attribute examine the state."
+  :description "Given an audit name and params examine the attributes."
   :license "Apache Version 2.0"
   :url "https://github.com/electric-it/tosca-lens"
   :scm {:name "git"
         :url "https://github.com/electric-it/tosca-lens"}  
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
                  [clj-yaml "0.4.0"]
                  [electric-it/clj-tosca "1.0.1"]
-                 [com.amazonaws/aws-java-sdk "1.10.26"]
                  [amazonica "0.3.43"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
-                   :source-paths ["dev"]}
-             :uberjar {:aot :all}})
+                   :source-paths ["dev"]}})
